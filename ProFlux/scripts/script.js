215337c97acf796862bd1d2a22aa351b661ef96b
NajDev <najib.achchaq@gmail.com>
2023-08-11 22:30:52 +0200

    proflux

--- ProFlux/scripts/script.js
+++ ProFlux/scripts/script.js
@@ -1,188 +1,305 @@
-
-// Cet événement s'active lorsque le document HTML est complètement chargé
 document.addEventListener("DOMContentLoaded", () => {
-    // On récupère des éléments spécifiques du DOM
+    // Code exécuté lorsque le DOM est complètement chargé
+
+    // --------------------------------------
+    // Premier formulaire
+    // --------------------------------------
+
+    // Sélection des éléments du premier formulaire
     const duplicateButton = document.getElementById("duplicateButton");
     const container = document.getElementById("container");
     const itemNameInput = document.querySelector("h1 input");
     const validationButton = document.querySelector(".validation");
     const affichageObjetsStockés = document.getElementById("affichageObjetsStockés");
-    
-    // Création d'une liste pour stocker les objets générés
-    const listeObjetsStockés = [];
     const objetsStockesDropdown = document.getElementById("objetsStockesDropdown");
-    const valeurDiv = document.querySelector(".valeur");
+    const valeursDiv = document.querySelectorAll(".valeur");
 
+    const listeObjetsStockés = [];
 
-// Lorsqu'une option est sélectionnée dans le menu déroulant
-objetsStockesDropdown.addEventListener("change", () => {
-    const selectedIndex = objetsStockesDropdown.value; // Index de l'objet sélectionné
-    const selectedObjet = listeObjetsStockés[selectedIndex]; // Objet correspondant
+    // Gestion de l'événement de changement dans le dropdown des objets stockés
+    objetsStockesDropdown.addEventListener("change", () => {
+        const selectedIndex = objetsStockesDropdown.value;
+        const selectedObjet = listeObjetsStockés[selectedIndex];
+        const selectedDescription = objetsStockesDropdown.options[objetsStockesDropdown.selectedIndex].text.split(" - ")[1];
 
-    // Obtenez la description sélectionnée dans le menu déroulant
-        // objetsStockesDropdown.options : Cela fait référence aux options disponibles dans le menu déroulant objetsStockesDropdown.
-        // objetsStockesDropdown.selectedIndex : C'est l'index de l'option actuellement sélectionnée dans le menu déroulant.
-        // .text : Cela renvoie le texte de l'option sélectionnée, c'est-à-dire le texte qui est affiché aux utilisateurs.
-        // .split(" - ") : C'est une méthode pour diviser une chaîne de caractères en plusieurs parties, en utilisant " - " comme séparateur. Dans ce contexte, cela divise le texte en deux parties : la première partie correspond au nom du composant et la deuxième partie correspond à la description de l'item.
-        // [1] : Cela sélectionne la deuxième partie résultant de la division, c'est-à-dire la description de l'item.
-    const selectedDescription = objetsStockesDropdown.options[objetsStockesDropdown.selectedIndex].text.split(" - ")[1];
+        valeursDiv.forEach((valeurDiv) => {
+            valeurDiv.textContent = "";
 
-    // Vide la div de la valeur
-    valeurDiv.textContent = "";
+            if (selectedObjet) {
+                const selectedItem = selectedObjet.items.find(item => item.description === selectedDescription);
+                if (selectedItem) {
+                    const selectedQuantite = selectedItem.quantité;
+                    valeurDiv.textContent = `${selectedQuantite}`;
+                } else {
+                    valeurDiv.textContent = "Item non trouvé dans l'objet";
+                }
+            }
+        });
+    });
 
-    if (selectedObjet) {
-        const selectedItem = selectedObjet.items.find(item => item.description === selectedDescription);
-        if (selectedItem) {
-            const selectedQuantite = selectedItem.quantité;
-            valeurDiv.textContent = `${selectedQuantite}`;
-        } else {
-            valeurDiv.textContent = "Item non trouvé dans l'objet";
+    // Gestion de l'ajout d'éléments au premier formulaire
+    duplicateButton.addEventListener("click", () => {
+        const existingItems = document.querySelectorAll(".item").length;
+
+        if (existingItems < 9) {
+            const newItem = document.querySelector(".item").cloneNode(true);
+
+            newItem.querySelector(".description").value = "";
+            newItem.querySelector(".inputQuantité").value = "";
+
+            container.appendChild(newItem);
+
+            if (existingItems + 1 === 9) {
+                duplicateButton.disabled = true;
+            }
         }
-    }
-});
+    });
 
-// Lorsque le bouton "Dupliquer" est cliqué
-duplicateButton.addEventListener("click", () => {
-
-    // je cree une variable contenant le nombre d'item cloné
-    const existingItems = document.querySelectorAll(".item").length;
-    
-    // si le nombre d'élément cloner est inférieur à 
-    if (existingItems < 9) {
-        // On clone l'élément ".item" pour créer une nouvelle entrée
-        const newItem = document.querySelector(".item").cloneNode(true);
-
-        // On vide les champs de saisie dans le nouvel élément cloné
-        newItem.querySelector(".description").value = "";
-        newItem.querySelector(".inputQuantité").value = "";
-
-        // On ajoute le nouvel élément cloné au conteneur
-        container.appendChild(newItem);
-
-        // Si le nombre d'éléments clonés atteint 10, désactive le bouton
-        if (existingItems + 1 === 9) {
-            duplicateButton.disabled = true;
-        }
-    }
-});
-
-    // Lorsque le bouton "Valider" est cliqué
+    // Gestion de la validation du premier formulaire
     validationButton.addEventListener("click", () => {
-        // On récupère le nom du composant depuis le champ de saisie
         const itemName = itemNameInput.value;
-        
-        // On sélectionne tous les éléments ".info" dans les éléments ".item"
         const itemElements = document.querySelectorAll(".item .info");
 
         const items = [];
-        // On parcourt chaque élément ".info"
         itemElements.forEach(itemElement => {
-            // On récupère les champs de saisie à l'intérieur de chaque élément ".info"
             const descriptionInput = itemElement.querySelector(".description");
             const quantitéInput = itemElement.querySelector(".inputQuantité");
 
-            // On extrait les valeurs des champs de saisie
             const description = descriptionInput.value;
             const quantité = quantitéInput.value;
 
-            // Si le champ "Description" n'est pas vide, on ajoute les valeurs à l'array "items"
             if (description.trim() !== "") {
                 items.push({ description, quantité });
             }
 
-            // On vide les champs de saisie dans chaque élément ".info"
             descriptionInput.value = "";
             quantitéInput.value = "";
-
             duplicateButton.disabled = false;
-            console.log(objetsStockesDropdown)
         });
 
-            // On crée un objet contenant les données entrées
-            const data = { nomDuComposant: itemName, items };
-            console.log(data);
+        const data = { nomDuComposant: itemName, items };
 
-            // On vide le champ de saisie du nom du composant après validation
-            itemNameInput.value = "";
+        itemNameInput.value = "";
+        listeObjetsStockés.push(data);
 
-            // On ajoute l'objet nouvellement créé à la liste des objets stockés
-            listeObjetsStockés.push(data);
-
-            // Suppression des éléments clonés du DOM
-            container.querySelectorAll(".item").forEach(item => {
-                if (item !== container.querySelector(".item")) {
-                    item.remove();
-                }
-            });
-
-            // Appel de la fonction pour afficher les objets stockés
-            afficherObjetsStockés();
-            console.log(listeObjetsStockés)
-        });
-
-        // Fonction pour afficher les objets stockés dans le DOM
-        function afficherObjetsStockés() {
-            affichageObjetsStockés.innerHTML = "";
-        
-            listeObjetsStockés.forEach(objet => {
-                // Création d'un élément <div> pour afficher l'objet
-                const objetDiv = document.createElement("div");
-                objetDiv.classList.add("objet"); // Ajouter une classe à la div
-                affichageObjetsStockés.appendChild(objetDiv);
-        
-                // Création d'un élément <div> pour le nom du composant
-                const nomDiv = document.createElement("div");
-                nomDiv.textContent = objet.nomDuComposant;
-                nomDiv.classList.add("nom-composant"); // Ajouter une classe au div du nom
-                objetDiv.appendChild(nomDiv);
-        
-                // Affichage de chaque élément de l'objet
-                objet.items.forEach(item => {
-                    const itemInfo = document.createElement("p");
-                    itemInfo.textContent = `${item.description}, ${item.quantité}`;
-                    itemInfo.classList.add("item-info"); // Ajouter une classe au div du nom
-                    objetDiv.appendChild(itemInfo);
-                });
-
-            
-        });
-
-            // Sélectionnez l'élément de la liste déroulante
-            const objetsStockesDropdown = document.getElementById("objetsStockesDropdown");
-
-            // Utilisé pour garder une trace des noms d'objets déjà ajoutés à la liste déroulante
-            const nomsObjetsAjoutés = [];
-
-            // Remplissez la liste déroulante avec les options des objets stockés
-            listeObjetsStockés.forEach((objet, index) => {
-                objet.items.forEach(item => {
-                    const nomObjetDescription = `${objet.nomDuComposant} - ${item.description}`;
-                    
-            // Vérifier si le nom de l'objet avec la description a déjà été ajouté
-            if (!nomsObjetsAjoutés.includes(nomObjetDescription)) {
-                // Vérifier si l'option n'existe pas déjà
-                const existingOption = Array.from(objetsStockesDropdown.options).find(option => option.textContent === nomObjetDescription);
-
-            // Si l'option n'existe pas déjà, ajoutez-la
-            if (!existingOption) {
-                // Créez un élément <option> pour chaque objet
-                const option = document.createElement("option");
-                option.value = index; // Utilisez l'index pour identifier l'objet sélectionné
-                option.textContent = nomObjetDescription;
-
-                // Ajoutez l'élément <option> à la liste déroulante
-                objetsStockesDropdown.appendChild(option);
-
-                // Ajoutez le nom de l'objet à la liste des noms ajoutés
-                nomsObjetsAjoutés.push(nomObjetDescription);
+        container.querySelectorAll(".item").forEach(item => {
+            if (item !== container.querySelector(".item")) {
+                item.remove();
             }
-           
-        }
-
-        });  
-
         });
 
+        afficherObjetsStockés();
+    });
+
+    // Affichage des objets stockés dans le DOM
+    function afficherObjetsStockés() {
+        affichageObjetsStockés.innerHTML = "";
+
+        listeObjetsStockés.forEach(objet => {
+            const objetDiv = document.createElement("div");
+            objetDiv.classList.add("objet");
+            affichageObjetsStockés.appendChild(objetDiv);
+
+            const nomDiv = document.createElement("div");
+            nomDiv.textContent = objet.nomDuComposant;
+            nomDiv.classList.add("nom-composant");
+            objetDiv.appendChild(nomDiv);
+
+            objet.items.forEach(item => {
+                const itemInfo = document.createElement("p");
+                itemInfo.textContent = `${item.description}, ${item.quantité}`;
+                itemInfo.classList.add("item-info");
+                objetDiv.appendChild(itemInfo);
+            });
+        });
+
+        remplirMenuDeroulant();
     }
 
+    // Remplissage du dropdown des objets stockés
+    function remplirMenuDeroulant() {
+        objetsStockesDropdown.innerHTML = '<option value="">Sélectionner composant</option>';
+        const nomsObjetsAjoutés = [];
+
+        listeObjetsStockés.forEach((objet, index) => {
+            objet.items.forEach(item => {
+                const nomObjetDescription = `${objet.nomDuComposant} - ${item.description}`;
+                if (!nomsObjetsAjoutés.includes(nomObjetDescription)) {
+                    const existingOption = Array.from(objetsStockesDropdown.options).find(option => option.textContent === nomObjetDescription);
+                    if (!existingOption) {
+                        const option = document.createElement("option");
+                        option.value = index;
+                        option.textContent = nomObjetDescription;
+                        objetsStockesDropdown.appendChild(option);
+                        nomsObjetsAjoutés.push(nomObjetDescription);
+                    }
+                }
+            });
+        });
+    }
+
+    // --------------------------------------
+    // Deuxième formulaire
+    // --------------------------------------
+
+    // Sélection des éléments du deuxième formulaire
+
+
+    // Deuxième formulaire
+        const duplicateButtonComponent = document.getElementById("duplicateButtonComponent");
+        const containerComponent = document.getElementById("containerComponent");
+        const listeComposés = [];
+        let clonedItemCount = 0; // Variable pour suivre le nombre d'éléments clonés
+
+        duplicateButtonComponent.addEventListener("click", () => {
+            const existingItemsComponent = document.querySelectorAll(".itemComponent").length;
+
+            if (existingItemsComponent < 9) {
+                const itemComponentTemplate = document.querySelector(".itemComponent");
+                const itemComponentClone = itemComponentTemplate.cloneNode(true);
+
+                clonedItemCount++;
+                const uniqueId = `itemComponentClone_${clonedItemCount}`;
+                itemComponentClone.id = uniqueId;
+
+                const deroulantClone = itemComponentClone.querySelector(".deroulant");
+                const dropdownComponent = deroulantClone.querySelector("select");
+                const valeurDivComponent = itemComponentClone.querySelector(".valeur");
+
+                dropdownComponent.selectedIndex = 0;
+                valeurDivComponent.textContent = "";
+
+                containerComponent.appendChild(itemComponentClone);
+
+                if (existingItemsComponent + 1 === 9) {
+                    duplicateButtonComponent.disabled = true;
+                }
+            
+                // Utilisation de l'ID unique pour le dropdownComponent comme référence
+                dropdownComponent.addEventListener("change", () => {
+                    updateValeurForClonedElement(valeurDivComponent, dropdownComponent); // Utilise le dropdownComponent actuel
+                });
+            }
+        });
+
+    function updateValeurForClonedElement(valeurDivComponent, dropdownComponent) {
+    const selectedIndex = dropdownComponent.selectedIndex;
+    const selectedDescription = dropdownComponent.options[selectedIndex].text.split(" - ")[1];
+    
+    valeurDivComponent.textContent = ""; // Réinitialisation de la valeur
+
+    if (selectedDescription.trim() !== "") {
+        // Parcourir la liste d'objets stockés pour trouver l'objet correspondant
+        for (const selectedObjet of listeObjetsStockés) {
+            const selectedItem = selectedObjet.items.find(item => item.description === selectedDescription);
+
+            if (selectedItem) {
+                const selectedQuantite = selectedItem.quantité;
+                valeurDivComponent.textContent = `${selectedQuantite}`;
+                break; // Sortir de la boucle dès que l'objet est trouvé
+            }
+        }
+
+        if (valeurDivComponent.textContent === "") {
+            valeurDivComponent.textContent = "Item non trouvé dans l'objet";
+        }
+    }
+}
+
+
+   // Fonction pour afficher les objets composés
+function afficherComposés() {
+    const affichageComposés = document.getElementById("affichageComposés");
+    affichageComposés.innerHTML = "";
+
+    listeComposés.forEach(composé => {
+        const composéDiv = document.createElement("div");
+        composéDiv.classList.add("composé");
+        affichageComposés.appendChild(composéDiv);
+
+        const nomDiv = document.createElement("div");
+        nomDiv.textContent = composé.nomDuComposé;
+        nomDiv.classList.add("nom-composé");
+        composéDiv.appendChild(nomDiv);
+
+        composé.items.forEach(item => {
+            const itemInfo = document.createElement("p");
+            itemInfo.textContent = `${item.description}, ${item.quantité}`;
+            itemInfo.classList.add("item-info");
+            composéDiv.appendChild(itemInfo);
+        });
+    });
+}
+
+const validationButtonComponent = document.querySelector(".validationComponent");
+
+// Gestion de la validation du deuxième formulaire
+validationButtonComponent.addEventListener("click", () => {
+    const composéName = document.querySelector("#containerComponent h1 input").value;
+    const itemComponentElements = document.querySelectorAll(".itemComponent .infoComponent");
+
+    const itemsComponent = [];
+    
+    itemComponentElements.forEach(itemComponentElement => {
+        const dropdownComponent = itemComponentElement.querySelector(".deroulant select");
+        const quantitéInput = itemComponentElement.querySelector(".inputQuantité");
+
+        const selectedDescription = dropdownComponent.options[dropdownComponent.selectedIndex].text.split(" - ")[1];
+        const selectedQuantité = quantitéInput.value;
+
+        if (selectedDescription.trim() !== "") {
+            itemsComponent.push({ description: selectedDescription, quantité: selectedQuantité });
+        }
+
+        quantitéInput.value = "";
+        dropdownComponent.selectedIndex = 0;
+        duplicateButtonComponent.disabled = false;
+    });
+
+    const composéData = { nomDuComposé: composéName, items: itemsComponent };
+
+    document.querySelector("#containerComponent h1 input").value = "";
+
+    listeComposés.push(composéData);
+
+    containerComponent.querySelectorAll(".itemComponent").forEach(itemComponent => {
+        if (itemComponent !== document.querySelector(".itemComponent")) {
+            itemComponent.remove();
+        }
+    });
+
+    // Appeler la fonction pour afficher les objets composés
+    afficherComposés();
+    console.log(listeComposés)
+    
+});
+
+    // Affichage des composés stockés dans le DOM
+    function afficherComposés() {
+        const affichageComposés = document.getElementById("affichageComposés");
+        affichageComposés.innerHTML = "";
+
+        listeComposés.forEach(composé => {
+            const composéDiv = document.createElement("div");
+            composéDiv.classList.add("composé");
+            affichageComposés.appendChild(composéDiv);
+
+            const nomDiv = document.createElement("div");
+            nomDiv.textContent = composé.nomDuComposé;
+            nomDiv.classList.add("nom-composé");
+            composéDiv.appendChild(nomDiv);
+
+            composé.items.forEach(item => {
+                const itemInfo = document.createElement("p");
+                itemInfo.textContent = `${item.description}, ${item.quantité}`;
+                itemInfo.classList.add("item-info");
+                composéDiv.appendChild(itemInfo);
+            });
+        });
+    }
+
+    // Appels initiaux
+    afficherObjetsStockés();
+    remplirMenuDeroulant();
+    
 });
