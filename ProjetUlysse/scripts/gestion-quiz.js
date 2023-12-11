5833dec9899030453d4ef67599bf27994f855544
NajibACHCHAQ <najib.achchaq@gmail.com>
2023-12-11 10:09:04 +0100

    modif page co

--- ProjetUlysse/scripts/gestion-quiz.js
+++ ProjetUlysse/scripts/gestion-quiz.js
@@ -70,3 +70,84 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.style.display = 'none';
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('candidatModal');
+
+    // Gestionnaire d'événements pour ouvrir la modal
+    const openModalButtons = document.querySelectorAll('.open-modal-button');
+    openModalButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+
+            modal.style.display = 'block';
+            console.log("click")
+        });
+    });
+
+    // Sélectionnez le bouton pour fermer la modal
+    const closeButton = document.querySelector('.close');
+    // Gestionnaire d'événements pour fermer la modal en cliquant sur le bouton de fermeture
+    closeButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    // // Gestionnaire d'événements pour fermer la modal en cliquant en dehors de la modal
+    // window.addEventListener('click', function (event) {
+    //     if (event.target === modal) {
+    //         modal.style.display = 'none';
+    //     }
+    // });
+
+    const modalQuestionForm = document.getElementById('modalCandidatForm');
+    modalQuestionForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        // Ajoutez le code pour traiter le formulaire ici
+        modal.style.display = 'none';
+    });
+});
+function envoyerEmail() {
+    var nom = document.getElementById('candidatName').textContent;
+    var prenom = document.getElementById('candidatLastName').textContent;
+    var mail = document.getElementById('candidatEmail').textContent;
+    var statutCandidat = document.getElementById('candidatStatut').textContent;
+    var suivi = document.getElementById('candidatStatut').textContent;
+    var statutSelect = document.getElementById('candidatSuivi').value;
+
+    // Construire le corps du mail
+    var sujet = 'Nouveau message pour ' + nom + ' ' + prenom;
+    var corpsMail = 'Nom: ' + nom + '\n';
+    corpsMail += 'Prénom: ' + prenom + '\n';
+    corpsMail += 'E-mail: ' + mail + '\n';
+    corpsMail += 'Statut Candidat: ' + statutCandidat + '\n';
+    corpsMail += 'Suivi: ' + suivi + '\n';
+    corpsMail += 'Nouveau statut: ' + statutSelect;
+
+    alert('E-mail envoyé avec succès:\n\n' + corpsMail);
+}
+
+
+
+    function ouvrirBoiteMail() {
+        // Récupérer les informations nécessaires
+        var nom = document.getElementById('candidatName').textContent;
+        var prenom = document.getElementById('candidatLastName').textContent;
+        var mail = document.getElementById('candidatEmail').textContent;
+        var statutCandidat = document.getElementById('candidatStatut').textContent;
+        var suivi = document.getElementById('candidatStatut').textContent;
+        var statutSelect = document.getElementById('candidatSuivi').value;
+    
+        // Construire le lien mailto avec les informations préremplies
+        var sujet = 'Nouveau message pour ' + nom + ' ' + prenom;
+        var corpsMail = 'Nom: ' + nom + '\n';
+        corpsMail += 'Prénom: ' + prenom + '\n';
+        corpsMail += 'E-mail: ' + mail + '\n';
+        corpsMail += 'Statut Candidat: ' + statutCandidat + '\n';
+        corpsMail += 'Suivi: ' + suivi + '\n';
+        corpsMail += 'Nouveau statut: ' + statutSelect;
+    
+        var lienMailto = 'mailto:' + encodeURIComponent(mail) +
+            '?subject=' + encodeURIComponent(sujet) +
+            '&body=' + encodeURIComponent(corpsMail);
+        window.location.href = lienMailto;
+    }
+    
+    
